c92ba4866aaca722de19036c94f0d9d0c4fa0cd4
Ale M <bomberstudios@gmail.com>
2023-10-06 18:42:36 +0200

    Added prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function Button({onClick, children}) {
-  return (
-    <button onClick={onClick}>
-      {children}
-    </button>
-  )
+function Button({ onClick, children }) {
+  return <button onClick={onClick}>{children}</button>;
 }
 
-function Card({children}) {
-  return (
-    <div className="card">
-      {children}
-    </div>
-  )
+function Card({ children }) {
+  return <div className="card">{children}</div>;
 }
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  const handleClick = () => setCount((count) => count + 1)
+  const handleClick = () => setCount((count) => count + 1);
 
   return (
     <>
       <h1>Vite + React</h1>
       <Card>
-        <Button onClick={handleClick}>
-          count is {count}
-        </Button>
+        <Button onClick={handleClick}>count is {count}</Button>
       </Card>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
